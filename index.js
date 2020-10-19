973667bb6098195c46f2548556e9d01da2d0ddb7
Gevorg Gasparyan <gevorg994@gmail.com>
2020-10-19 17:01:38 +0400

    feat: Detect QR code from image

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ import {NativeModules, processColor} from 'react-native';
 
 const {RNQrGenerator} = NativeModules;
 
+export type QRCodeGenerateOptions = {
+  value: string,
+  backgroundColor?: string,
+  color?: string,
+  width?: number,
+  height?: number,
+  base64?: boolean,
+};
+
+export type QRCodeDetectOptions = {
+  uri?: string,
+  base64?: string,
+};
+
 export default {
-  generate: options => {
+  generate: (options: QRCodeGenerateOptions) => {
     const {value, backgroundColor, color} = options;
     if (!value) {
       return Promise.reject('Property "value" is missing');
@@ -15,6 +29,22 @@ export default {
     };
     return new Promise((resolve, reject) => {
       RNQrGenerator.generate(
+        qrOptions,
+        error => reject(error),
+        data => resolve(data),
+      );
+    });
+  },
+  detect: (options: QRCodeDetectOptions) => {
+    const {uri, base64} = options;
+    if (!uri && !base64) {
+      return Promise.reject('Property "uri" or "base64" are missing');
+    }
+    const qrOptions = {
+      ...options,
+    };
+    return new Promise((resolve, reject) => {
+      RNQrGenerator.detect(
         qrOptions,
         error => reject(error),
         data => resolve(data),
